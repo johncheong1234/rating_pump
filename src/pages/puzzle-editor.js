aaf82998cfb341c150f2266a8272d8eb81a40f55
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-29 13:56:26 +0800

    initial board editing

--- src/pages/puzzle-editor.js
+++ src/pages/puzzle-editor.js
@@ -3,7 +3,7 @@ import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import { atom, useAtom } from "jotai";
 
-const fenString = atom("1k6/8/8/8/8/8/2K5/8 w - - 0 1");
+const fenString = atom("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w K - 0 1");
 const chessBoardAtom = atom(
     (get) => new Chess(get(fenString)),
 );
@@ -19,7 +19,7 @@ const colorToPlayAtom = atom(
     },
     (get, set, color) => {
         // edit fenString to change the color to play
-        if(color === "white") {
+        if (color === "white") {
             set(fenString, get(fenString).replace(" b ", " w "))
             set(fenStringCopyAtom, get(fenString).replace(" b ", " w "))
         } else {
@@ -49,12 +49,8 @@ export default function PlayRandomMoveEngine() {
     }
 
     function onDrop(sourceSquare, targetSquare, piece) {
-        // makeAMove({
-        //     from: sourceSquare,
-        //     to: targetSquare,
-        //     promotion: "q", // always promote to a queen for example simplicity
-        // });
-        console.log("sourceSquare", sourceSquare, "targetSquare", targetSquare, "piece", piece)
+
+        modifyFenStringWithNewPiece(fenStringCopy, sourceSquare, targetSquare, piece)
     }
 
     function validateFenValue(fen) {
@@ -79,13 +75,97 @@ export default function PlayRandomMoveEngine() {
         }
     }
 
-    function handleColorChange(e){
+    function handleColorChange(e) {
 
         console.log("e.target.value", e.target.value)
         setColorToPlay(e.target.value)
     }
 
-   
+    function modifyFenStringWithNewPiece(existingFenString, sourceSquare, targetSquare, piece) {
+        const stringRelevantToBoard = existingFenString.split(" ")[0];
+        const leftOverFenString = existingFenString.split(" ").slice(1).join(" ");
+        const board = decodeFEN(stringRelevantToBoard);
+        const sourceRank = 8 - parseInt(sourceSquare[1]);
+        const sourceFile = sourceSquare.charCodeAt(0) - 97;
+        const targetRank = 8 - parseInt(targetSquare[1]);
+        const targetFile = targetSquare.charCodeAt(0) - 97;
+        const colour = piece[0] === "w" ? "white" : "black";
+        board[sourceRank][sourceFile] = null;
+        board[targetRank][targetFile] = colour === 'white' ? piece[1].toUpperCase() : piece[1].toLowerCase();
+        console.log(board, ' after')
+        const newFenString = encodeFEN(board) + " " + leftOverFenString;
+        console.log(newFenString, ' newFenString')
+        if (validateFenValue(newFenString)) {
+            setFenStringCopy(newFenString)
+            setFenString(newFenString)
+        } else {
+            setFenStringCopy(game.fen())
+        }
+    }
+
+    function decodeFEN(fenString) {
+
+        // Split the board part of the FEN string into its individual ranks
+        const ranks = fenString.split("/");
+
+        // Initialize an empty 8x8 chessboard array
+        const chessboard = Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => null));
+
+        // Loop through each rank and file to populate the chessboard
+        for (let rank = 0; rank < 8; rank++) {
+            let file = 0;
+            for (let i = 0; i < ranks[rank].length; i++) {
+                const char = ranks[rank][i];
+                if (!isNaN(char)) {
+                    // If the character is a number, skip that many files
+                    file += parseInt(char, 10);
+                } else {
+                    // Otherwise, the character is a piece
+                    chessboard[rank][file] = char;
+                    file++;
+                }
+            }
+        }
+
+        return chessboard;
+    }
+
+    function encodeFEN(chessboard) {
+        // Initialize an empty string to build the FEN string
+        let fenString = "";
+
+        // Loop through each rank
+        for (let rank = 0; rank < 8; rank++) {
+            let emptySquares = 0;
+            // Loop through each file in the rank
+            for (let file = 0; file < 8; file++) {
+                const piece = chessboard[rank][file];
+                if (piece === null) {
+                    // If the square is empty, increment the empty squares counter
+                    emptySquares++;
+                } else {
+                    // If the square is not empty, add the appropriate character to the FEN string
+                    if (emptySquares > 0) {
+                        fenString += emptySquares;
+                        emptySquares = 0;
+                    }
+                    fenString += piece;
+                }
+            }
+            // If there are any remaining empty squares, add them to the FEN string
+            if (emptySquares > 0) {
+                fenString += emptySquares;
+            }
+            // Add a slash after each rank except for the last one
+            if (rank < 7) {
+                fenString += "/";
+            }
+
+        }
+        return fenString;
+    }
+
+
     return (
         <div style={{
             display: "flex",
