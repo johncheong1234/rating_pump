f0366f8003eadf114b285411c19776713a59f337
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-29 08:31:22 +0800

    chessboard initial

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,9 +1,11 @@
-import React from "react"
+import { useState } from "react";
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
 
-export default function Test() {
-    return (
-        <div>
-            <h1>Test</h1>
-        </div>
-    )
+export default function PlayRandomMoveEngine() {
+    const [game, setGame] = useState(new Chess(
+        'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - - 0 19'
+    ));
+
+    return <Chessboard position={game.fen()} />;
 }
