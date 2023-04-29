eb9658d39f6cbe1196a7060c5a46c2cc001c9e97
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-29 08:15:48 +0800

    test

--- src/pages/api/algorithms.js
+++ src/pages/api/algorithms.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
     try {
         const client = await clientPromise;
         const db = client.db("lalgo");
- 
+        console.log("db", db)
         const algorithms = await db
             .collection("algorithms")
             .find({})
