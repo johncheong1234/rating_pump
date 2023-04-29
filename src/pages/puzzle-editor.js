1e47ca7efb6926ee1d930f01a24a993102912f95
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-29 13:10:37 +0800

    change colour

--- src/pages/puzzle-editor.js
+++ src/pages/puzzle-editor.js
@@ -8,11 +8,32 @@ const chessBoardAtom = atom(
     (get) => new Chess(get(fenString)),
 );
 const fenStringCopyAtom = atom("")
+const colorToPlayAtom = atom(
+    (get) => {
+        // get fenString and return the color to play
+        if (get(fenString).split(" ")[1] === "w") {
+            return "white"
+        } else {
+            return "black"
+        }
+    },
+    (get, set, color) => {
+        // edit fenString to change the color to play
+        if(color === "white") {
+            set(fenString, get(fenString).replace(" b ", " w "))
+            set(fenStringCopyAtom, get(fenString).replace(" b ", " w "))
+        } else {
+            set(fenString, get(fenString).replace(" w ", " b "))
+            set(fenStringCopyAtom, get(fenString).replace(" w ", " b "))
+        }
+    }
+)
 
 export default function PlayRandomMoveEngine() {
     const [game, setGame] = useAtom(chessBoardAtom);
     const [, setFenString] = useAtom(fenString);
     const [fenStringCopy, setFenStringCopy] = useAtom(fenStringCopyAtom);
+    const [colorToPlay, setColorToPlay] = useAtom(colorToPlayAtom);
 
     // set fenStringCopy to fenString on first render
     if (fenStringCopy === "") {
@@ -27,12 +48,13 @@ export default function PlayRandomMoveEngine() {
         return result; // null if the move was illegal, the move object if the move was legal
     }
 
-    function onDrop(sourceSquare, targetSquare) {
-        makeAMove({
-            from: sourceSquare,
-            to: targetSquare,
-            promotion: "q", // always promote to a queen for example simplicity
-        });
+    function onDrop(sourceSquare, targetSquare, piece) {
+        // makeAMove({
+        //     from: sourceSquare,
+        //     to: targetSquare,
+        //     promotion: "q", // always promote to a queen for example simplicity
+        // });
+        console.log("sourceSquare", sourceSquare, "targetSquare", targetSquare, "piece", piece)
     }
 
     function validateFenValue(fen) {
@@ -49,7 +71,7 @@ export default function PlayRandomMoveEngine() {
         setFenStringCopy(e.target.value)
     }
 
-    function handleOnBlurFenStringCopy(e){
+    function handleOnBlurFenStringCopy(e) {
         if (validateFenValue(e.target.value)) {
             setFenString(e.target.value)
         } else {
@@ -57,19 +79,51 @@ export default function PlayRandomMoveEngine() {
         }
     }
 
+    function handleColorChange(e){
 
+        console.log("e.target.value", e.target.value)
+        setColorToPlay(e.target.value)
+    }
+
+   
     return (
-        <div>
-            <Chessboard position={game.fen()} onPieceDrop={onDrop} boardWidth={800} />
-            <div id='fen-string-container'>
-                <input
-                    style={{
-                        width: "800px",
-                        marginTop: "20px"
-                    }}
-                    type="text" value={fenStringCopy} 
-                    onChange={handleFenStringCopyChange} 
-                    onBlur={handleOnBlurFenStringCopy}/>
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+        }}
+            id="main-container"
+        >
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "fit-content",
+                }}
+            >
+                <Chessboard position={game.fen()} onPieceDrop={onDrop} boardWidth={
+                    600
+                } />
+                <div id='fen-string-container'>
+                    <input
+                        style={{
+                            width: "100%",
+                            marginTop: "20px"
+                        }}
+                        type="text" value={fenStringCopy}
+                        onChange={handleFenStringCopyChange}
+                        onBlur={handleOnBlurFenStringCopy} />
+                </div>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+            >
+                <select value={colorToPlay} onChange={handleColorChange}>
+                    <option value="white">White to play</option>
+                    <option value="black">Black to play</option>
+                </select>
             </div>
         </div>
     );
